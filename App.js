612b796cac8ee40f18ad793de627f7c6e529eed2
unknown <rameshmadara@gmail.com>
2023-12-24 00:07:03 +0530

    2

--- App.js
+++ App.js
@@ -1,33 +1,67 @@
-import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Battery from 'expo-battery';
+import { useEffect, useState } from 'react';
+import { Audio } from 'expo-av';
 
 export default function App() {
-  const [batteryPercentage, setBatteryPercentage] = useState(null);
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [batteryInfo, setBatteryInfo] = useState(undefined);
+  const [sound, setSound] = useState(null);
+  const [played, setPlayed] = useState(false);
 
   useEffect(() => {
-    const getBatteryPercentage = async () => {
-      try {
-        const batteryInfo = await Battery.getBatteryStateAsync();
-        if (batteryInfo.isAvailable) {
-          setBatteryPercentage(batteryInfo.batteryPercentage);
-        } else {
-          setBatteryPercentage('Battery info not accessible');
+    async function checkAvailability() {
+      const isBatteryAvailable = await Battery.isAvailableAsync();
+      setIsAvailable(isBatteryAvailable);
+      if (isBatteryAvailable) {
+        const batteryInfoLoaded = await Battery.getPowerStateAsync();
+        setBatteryInfo(batteryInfoLoaded);
+
+        if (Math.round(batteryInfoLoaded.batteryLevel * 100) === 86) {
+          playSound(); // Play sound if battery level is 86%
+          setPlayed(true); // Set played to true to prevent repeated plays
         }
-      } catch (error) {
-        console.error('Error fetching battery info:', error.message);
-        setBatteryPercentage('Error fetching battery info');
+      }
+    }
+    checkAvailability();
+  }, [played]);
+
+  useEffect(() => {
+    return () => {
+      if (sound) {
+        sound.unloadAsync(); // Unload the sound when the component unmounts
       }
     };
+  }, [sound]);
 
-    getBatteryPercentage();
-  }, []);
+  const playSound = async () => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        require('./assets/notification_sound.mp3') // Replace with your sound file
+      );
+      setSound(sound);
+      await sound.playAsync();
+    } catch (error) {
+      console.error('Error playing sound:', error);
+    }
+  };
+
+  const showBatteryInfo = () => {
+    if (!batteryInfo) return <Text>Battery info not loaded</Text>;
+    return (
+      <View>
+        <Text style={styles.goldText}>Low Power Mode: {batteryInfo.lowPowerMode ? 'Yes' : 'No'}</Text>
+        <Text style={styles.goldText}>Battery Level: {Math.round(batteryInfo.batteryLevel * 100)}%</Text>
+        <Text style={styles.goldText}>Battery State: {batteryInfo.batteryState}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your application!</Text>
-      <Text>Battery Percentage: {batteryPercentage ?? 'Loading...'}</Text>
+      <Text style={styles.goldText}>{isAvailable ? 'Battery Details: ' : 'Battery info unavailable'}</Text>
+      {showBatteryInfo()}
       <StatusBar style="auto" />
     </View>
   );
@@ -36,9 +70,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#777',
+    backgroundColor: '#fff', // Black background color
     alignItems: 'center',
     justifyContent: 'center',
-    
+  },
+  goldText: {
+    color: 'coral', // Gold text color
+    marginBottom: 8, // Add space between text elements
   },
 });
